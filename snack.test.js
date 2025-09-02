efbe56c2de80dd2cb1ff32dd1a08fc12ac1655e0
andrea-mosca <andrea.moscateli@gmail.com>
2025-09-02 17:19:11 +0200

    describe

--- snack.test.js
+++ snack.test.js
@@ -5,43 +5,6 @@ const {
   isPalindrome,
   findPostById,
 } = require("./snack.js");
-// snack 1
-test("La funzione getInitials restituisce le iniziali di un nome completo", () => {
-  expect(getInitials("Andrea Moscatelli")).toBe("AM");
-  expect(getInitials("Luca Bianchi")).toBe("LB");
-});
-
-// // snack 2
-
-// test("La funzione createSlug restituisce una stringa in lowercase.", () => {
-//   expect(createSlug("JAVASCRIPT")).toBe("javascript");
-//   expect(createSlug("Javascript")).toBe("javascript");
-//   expect(createSlug("JAVA SCRIPT")).toBe("java script");
-// });
-
-// snack 3
-
-test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
-  expect(average([10, 12, 11])).toBe(11);
-});
-
-// snack 4
-test("La funzione createSlug sostituisce gli spazi con -.", () => {
-  expect(createSlug("Questo è un test")).toBe("questo-è-un-test");
-});
-
-// snack 5
-
-test("La funzione isPalindrome verifica se una stringa è un palindromo.", () => {
-  expect(isPalindrome("yamamay")).toBeTruthy();
-});
-
-// snack 6
-test("La funzione createSlug lancia un errore se il titolo è vuoto o non valido.", () => {
-  expect(() => createSlug("")).toThrow("il titolo non può essere vuoto");
-});
-
-// snack 7
 
 const posts = [
   {
@@ -71,18 +34,61 @@ const posts = [
   },
 ];
 
-test("La funzione findPostById restituisce il post corretto dato l’array di post e l’id", () => {
-  const post = {
-    id: 1,
-    title: "Introduzione a JavaScript",
-    slug: "introduzione-a-javascript",
-  };
-  expect(findPostById(posts, 1)).toEqual(post);
-  expect(() => findPostById(post, "uno")).toThrow(
-    "L'ID passato non è un valore numerico"
-  );
-  expect(findPostById(posts, 10)).toBe(null);
-  expect(() => findPostById([12, 356, 4], 1)).toThrow(
-    "è stato passato un array errato"
-  );
+// * ORGANIZZAZIONE CON DESCRIBE
+
+// * STRINGHE
+describe("MANIPOLATORE DI STRINGHE", () => {
+  // snack 1
+  test("La funzione getInitials restituisce le iniziali di un nome completo", () => {
+    expect(getInitials("Andrea Moscatelli")).toBe("AM");
+    expect(getInitials("Luca Bianchi")).toBe("LB");
+  });
+
+  // snack 5
+  test("La funzione isPalindrome verifica se una stringa è un palindromo.", () => {
+    expect(isPalindrome("yamamay")).toBeTruthy();
+  });
+});
+
+// * ARRAY
+describe("MANIPOLATORE DI ARRAY", () => {
+  // snack 3
+  test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+    expect(average([10, 12, 11])).toBe(11);
+  });
+  //   snack 7
+  test("La funzione findPostById restituisce il post corretto dato l’array di post e l’id", () => {
+    const post = {
+      id: 1,
+      title: "Introduzione a JavaScript",
+      slug: "introduzione-a-javascript",
+    };
+    expect(findPostById(posts, 1)).toEqual(post);
+    expect(() => findPostById(post, "uno")).toThrow(
+      "L'ID passato non è un valore numerico"
+    );
+    expect(findPostById(posts, 10)).toBe(null);
+    expect(() => findPostById([12, 356, 4], 1)).toThrow(
+      "è stato passato un array errato"
+    );
+  });
+});
+
+// * SLUG
+describe("CREATORE DI SLUG", () => {
+  // snack 2
+  test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+    expect(createSlug("JAVASCRIPT")).toBe("javascript");
+    expect(createSlug("Javascript")).toBe("javascript");
+    expect(createSlug("JAVA SCRIPT")).toBe("java-script");
+  });
+
+  // snack 4
+  test("La funzione createSlug sostituisce gli spazi con -.", () => {
+    expect(createSlug("Questo è un test")).toBe("questo-è-un-test");
+  });
+  // snack 6
+  test("La funzione createSlug lancia un errore se il titolo è vuoto o non valido.", () => {
+    expect(() => createSlug("")).toThrow("il titolo non può essere vuoto");
+  });
 });
